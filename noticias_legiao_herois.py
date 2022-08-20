e750902236bad10ee5c39b4ce7a69c999c26fa41
Diego Araujo <diego_shakan@hotmail.com>
2022-08-20 13:32:44 -0300

    adicionando imagens quando houverem mais de uma

--- noticias_legiao_herois.py
+++ noticias_legiao_herois.py
@@ -21,6 +21,14 @@ for link in links:
   subtitle = content_post.find('p', attrs={'class': 'post__tagline'}).text
   author = content_post.find('div', attrs={'class': 'post__details__info__feature'}).find('a').text
   texts = content_post.find_all('p')
+  datetime = content_post.find('time', attrs={'class': 'timeago'})['datetime']
+  
+  main_image = content_post.find('img', attrs={'class': 'article-cover__image'})['srcset']
+  main_image = main_image.split()
+  main_image = main_image[len(main_image) - 2]
+
+  # falta procurar as outras imagens -> referência: https://www.legiaodosherois.com.br/2022/death-note-borracha-morte.html
+  # more_image = content_post.select('div.wp-caption.aligncenter.lazy-parent.lazy-parent--multi.child-loaded')
 
   texts.pop(0)
   texts.pop()
@@ -31,8 +39,11 @@ for link in links:
 
   full_text = " ".join(full_text)
 
-  posts_ready.append([title, subtitle, author, full_text, link])
+  posts_ready.append([title, subtitle, author, full_text, datetime, main_image, link])
 
-posts = pd.DataFrame(posts_ready, columns=['Título', 'Subtítulo', 'Author', 'Texto', 'link'])
+  print(more_image)
 
-posts.to_csv('posts_legiao_herois.csv', index=False)
+
+# posts = pd.DataFrame(posts_ready, columns=['Título', 'Subtítulo', 'Author', 'Texto', 'Data Postagem', 'Capa', 'link'])
+
+# posts.to_csv('posts_legiao_herois.csv', index=False)
